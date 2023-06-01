d0ae4da073b653e07ceec481d97d1d91a3a3ac12
KonotopOlga <ol.konotop@yandex.ru>
2023-05-31 22:57:10 -0300

    добавили22

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -4,6 +4,21 @@ const Main = () => {
     return <>
         <News/>
         <h1>Главная страница</h1>
+        <div>
+        <img class="Banner" src='https://techport-st.cdn.ngenix.net/_mod_files/ce_images/eshop/p1028166-3mw.jpg'/>
+      <a href="#action">Акция</a>
+      <button>Заказать</button>
+    </div>
+    <div>
+        <img class="Banner_2" src='http://new.faunavet.ru/uploads/catalog/products/mini/699.jpg'/>
+      <a href="#action">Акция</a>
+      <button>Заказать</button>
+    </div>
+    <div>
+        <img class="Banner_3" src='https://dogworry.ru/wp-content/uploads/2018/11/chem-kormit-francuzskogo-buldoga-chtoby-u-nego-ne-bylo-allergii.jpg'/>
+      <a href="#action">Акция</a>
+      <button>Заказать</button>
+    </div>
     </>
 }
 
